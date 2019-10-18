9f5735b70ce8748fdf9c210e72b86f8ee0338035
mtorres <mtorres@kdu.cl>
2019-10-18 18:03:56 -0300

    INICIAL

---
+++ src/main/java/org/tensa/visualizador/SpacePolygon.java
@@ -0,0 +1,15 @@
+package org.tensa.visualizador;
+
+import java.awt.Color;
+import java.awt.Polygon;
+
+/**
+ *
+ * @author lorenzo
+ */
+public class SpacePolygon extends Polygon {
+    
+    public Color color = Color.BLACK;
+    public Double distance = 0.0;
+    
+}
